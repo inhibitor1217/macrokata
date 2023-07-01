8481fe50b8d9d85242c75ac1333a39dd7c1d601b
inhibitor1217 <inhibitor@kaist.ac.kr>
2023-07-01 21:31:29 +0900

    solve: 06

--- exercises/06_repetition/main.rs
+++ exercises/06_repetition/main.rs
@@ -4,7 +4,11 @@ fn print_success() {
 }
 ////////// DO NOT CHANGE ABOVE HERE /////////
 
-// TODO: create `if_any!()` macro.
+macro_rules! if_any {
+    ($($cond:expr),+; $block:block) => {
+        if $($cond)||+ $block
+    };
+}
 
 ////////// DO NOT CHANGE BELOW HERE /////////
 
